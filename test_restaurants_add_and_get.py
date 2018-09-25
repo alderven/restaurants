93099ea27dd3feb84e8c6a7fcc734c0a34fe75dd
alderven <alderven@gmail.com>
2018-09-25 13:25:06 +0300

    Improve test and regenerate Allure report

--- test_restaurants_add_and_get.py
+++ test_restaurants_add_and_get.py
@@ -12,21 +12,31 @@ from swagger_server.models.address import Address  # noqa: E501
 class TestDefaultController(BaseTestCase):
 
     def test_restaurants_get(self):
+
         query_string = [('id', 56), ('zipcode', 'zipcode_example')]
-        response = self.client.open(
-            '/restaurants',
-            method='GET',
-            content_type='application/json',
-            query_string=query_string)
-        self.assert200(response, 'Response body is : ' + response.data.decode('utf-8'))
+
+        with pytest.allure.step('1. Get restaurant with following parameters: {}'.format(query_string)):
+            response = self.client.open(
+                '/restaurants',
+                method='GET',
+                content_type='application/json',
+                query_string=query_string)
+
+        with pytest.allure.step('2. Check, that response code is 200'):
+            self.assert200(response, 'Response body is : ' + response.data.decode('utf-8'))
 
     def test_restaurants_add(self):
-        address = Address(zipcode='12345', city='Moscow', street='Lenina', latitude=1, longitude=1)
-        restaurant = Restaurant(id=12345, name='MyRest1', address=address, owner='Me', emails=['rest@ya.ru'],
-                                phones=['+79123456789'])
-        response = self.client.open(
-            '/restaurants',
-            method='POST',
-            data=json.dumps(restaurant),
-            content_type='application/json')
-        self.assert200(response, 'Response body is : ' + response.data.decode('utf-8'))
+
+        with pytest.allure.step('1. Create restaurant'):
+
+            address = Address(zipcode='12345', city='Moscow', street='Lenina', latitude=1, longitude=1)
+            restaurant = Restaurant(id=12345, name='MyRest1', address=address, owner='Me', emails=['rest@ya.ru'],
+                                    phones=['+79123456789'])
+            response = self.client.open(
+                '/restaurants',
+                method='POST',
+                data=json.dumps(restaurant),
+                content_type='application/json')
+
+        with pytest.allure.step('2. Check, that response code is 200'):
+            self.assert200(response, 'Response body is : ' + response.data.decode('utf-8'))
